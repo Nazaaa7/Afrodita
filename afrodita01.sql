45d1fc618ae82e219b1e9c5e1c49b3ed6f6dc3f5
Nazarena Garcia <nazagarcia132@gmail.com>
2024-06-23 18:29:46 -0300

    bd

--- afrodita01.sql
+++ afrodita01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-06-2024 a las 02:13:12
+-- Tiempo de generación: 23-06-2024 a las 23:24:38
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -32,6 +32,16 @@ CREATE TABLE `cargo_personal` (
   `cargo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Volcado de datos para la tabla `cargo_personal`
+--
+
+INSERT INTO `cargo_personal` (`idCargo`, `cargo`) VALUES
+(1, 'Administrador'),
+(2, 'Recepcionista'),
+(3, 'Empleado1'),
+(4, 'Cajera');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +68,31 @@ CREATE TABLE `insumos` (
   `idTipoInsumo` int(4) DEFAULT NULL,
   `idProveedor` int(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personal`
+--
+
+CREATE TABLE `personal` (
+  `idPersonal` int(4) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `dni` varchar(20) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `sexo` enum('Femenino','Masculino') NOT NULL,
+  `idCargo` int(4) NOT NULL,
+  `beneficios` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `personal`
+--
+
+INSERT INTO `personal` (`idPersonal`, `nombres`, `apellidos`, `dni`, `fecha_nac`, `telefono`, `sexo`, `idCargo`, `beneficios`) VALUES
+(3, 'naza', 'garcia', '46115454', '2005-06-09', '3704774295', 'Femenino', 1, 'Administrador');
 
 -- --------------------------------------------------------
 
@@ -100,6 +135,21 @@ CREATE TABLE `servicios` (
   `idTipoServicio` int(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`idServicio`, `nombreServicio`, `descripcion`, `idTipoServicio`) VALUES
+(1, 'manicura', 'manicura', 1),
+(2, 'esmaltado', 'esmaltado semipermatente', 1),
+(3, 'corte de cabello', 'corte de cabello', 2),
+(4, 'pedicura basica', 'no', 3),
+(5, 'esmalte', 'llo', 3),
+(6, 'maquillaje de eventos', '', 4),
+(7, 'maquillaje de noche', '', 4),
+(8, 'facial basico', '', 5),
+(9, 'tratamiento anticelulitis', '', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -110,6 +160,17 @@ CREATE TABLE `tipos_servicios` (
   `idTipoServicio` int(4) NOT NULL,
   `tipoServicio` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipos_servicios`
+--
+
+INSERT INTO `tipos_servicios` (`idTipoServicio`, `tipoServicio`) VALUES
+(1, 'manicura'),
+(2, 'pedicura'),
+(3, 'peluqueria'),
+(4, 'maquillaje'),
+(5, 'tratatmientos');
 
 -- --------------------------------------------------------
 
@@ -144,8 +205,18 @@ CREATE TABLE `turnos` (
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
   `idUsuario` int(4) DEFAULT NULL,
-  `idServicio` int(4) DEFAULT NULL
+  `idServicio` int(4) DEFAULT NULL,
+  `idPersonal` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `turnos`
+--
+
+INSERT INTO `turnos` (`idTurno`, `fecha`, `hora`, `idUsuario`, `idServicio`, `idPersonal`) VALUES
+(4, '2024-05-29', '21:57:00', NULL, NULL, 3),
+(6, '2024-06-05', '20:08:00', NULL, NULL, 3),
+(7, '2024-06-07', '18:25:00', NULL, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -187,6 +258,14 @@ ALTER TABLE `insumos`
   ADD PRIMARY KEY (`idInsumo`),
   ADD KEY `idTipoInsumo` (`idTipoInsumo`),
   ADD KEY `idProveedor` (`idProveedor`);
+
+--
+-- Indices de la tabla `personal`
+--
+ALTER TABLE `personal`
+  ADD PRIMARY KEY (`idPersonal`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD KEY `idCargo` (`idCargo`);
 
 --
 -- Indices de la tabla `proveedor`
@@ -232,7 +311,8 @@ ALTER TABLE `tipo_insumos`
 ALTER TABLE `turnos`
   ADD PRIMARY KEY (`idTurno`),
   ADD KEY `idUsuario` (`idUsuario`),
-  ADD KEY `idServicio` (`idServicio`);
+  ADD KEY `idServicio` (`idServicio`),
+  ADD KEY `idPersonal` (`idPersonal`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -249,7 +329,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `cargo_personal`
 --
 ALTER TABLE `cargo_personal`
-  MODIFY `idCargo` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCargo` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_turno`
@@ -262,6 +342,12 @@ ALTER TABLE `detalle_turno`
 --
 ALTER TABLE `insumos`
   MODIFY `idInsumo` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `personal`
+--
+ALTER TABLE `personal`
+  MODIFY `idPersonal` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
@@ -279,13 +365,13 @@ ALTER TABLE `registro_insumos`
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  MODIFY `idServicio` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idServicio` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `tipos_servicios`
 --
 ALTER TABLE `tipos_servicios`
-  MODIFY `idTipoServicio` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTipoServicio` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `tipos_usuario`
@@ -303,7 +389,7 @@ ALTER TABLE `tipo_insumos`
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `idTurno` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTurno` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -330,6 +416,12 @@ ALTER TABLE `insumos`
   ADD CONSTRAINT `insumos_ibfk_2` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`);
 
 --
+-- Filtros para la tabla `personal`
+--
+ALTER TABLE `personal`
+  ADD CONSTRAINT `personal_ibfk_1` FOREIGN KEY (`idCargo`) REFERENCES `cargo_personal` (`idCargo`);
+
+--
 -- Filtros para la tabla `registro_insumos`
 --
 ALTER TABLE `registro_insumos`
@@ -346,7 +438,8 @@ ALTER TABLE `servicios`
 --
 ALTER TABLE `turnos`
   ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
-  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`idServicio`) REFERENCES `servicios` (`idServicio`);
+  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`idServicio`) REFERENCES `servicios` (`idServicio`),
+  ADD CONSTRAINT `turnos_ibfk_3` FOREIGN KEY (`idPersonal`) REFERENCES `personal` (`idPersonal`);
 
 --
 -- Filtros para la tabla `usuarios`
